fb34be0f570ea333287287ba6b4f9949f813f1f3
Vyom Raval <ravalvyom17@gmail.com>
2023-02-22 21:09:34 +0530

    first cpp inheritance

---
+++ c1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int multi(int,int);
+
+//float div(float,float);
+int main()
+{
+    int a,b;
+    //float c,d;
+
+    cout<<"Enter the value of a";
+    cin>>a;
+
+    cout<<"Enter the value of b";
+    cin>>b;
+/*
+    cout<<"Enter the value of c";
+    cin>>c;
+
+    cout<<"Enter the value of d";
+    cin>>d;
+*/  
+10
+    cout<<multi(a,b)<<endl;
+
+    //cout<<div(c,d)<<endl;
+    return 0;
+}
+
+int multi(int x,int y)
+{
+    return x*y;
+}
+.
+//float div(float p,float q)
+//{
+  //  return (p/q);
+//}
